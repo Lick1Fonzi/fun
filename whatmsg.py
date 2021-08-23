5df8e92a2df9d5ca71a05b127402cc767ff96009
Lick1Fonzi <77489503+Lick1Fonzi@users.noreply.github.com>
2021-08-23 12:44:30 +0200

    Update whatmsg.py

--- whatmsg.py
+++ whatmsg.py
@@ -4,10 +4,10 @@ ora =int( input("inserisci ora") )
 minuto =int( input("inserisci minuto") )
 mes1 = "Gentile reminder delle "
 mes2 = ": torna al nido presto, hai la nostra pizza"
-cell = "+393450556692"
+cell = "+39775643676"	#example number
 while(1):
 	print("invio messaggio in attesa")
-	#whatsapp web must be open 
+	#whatsapp web must be open for the message to be sent
 	pywhatkit.sendwhatmsg(cell, mes1+str(ora)+" e "+str(minuto)+mes2, ora,minuto)
 	print("messaggio inviato")
 	minuto+=5
